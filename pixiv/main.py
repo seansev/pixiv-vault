59d3911d6574461dd8f349e10dcb3b43e0e84943
Sean S. Williams <mail@seansev.com>
2026-04-23 03:11:24 -0400

    Move pixiv main into def main()

--- pixiv/main.py
+++ pixiv/main.py
@@ -2,12 +2,16 @@ import dotenv
 import os
 from pixivpy3 import AppPixivAPI
 
-envfile = dotenv.find_dotenv()
-dotenv.load_dotenv(envfile)
+def main():
+    envfile = dotenv.find_dotenv()
+    dotenv.load_dotenv(envfile)
 
-refresh_token = os.environ["REFRESH_TOKEN"]
-user_id = os.environ["USER_ID"]
+    refresh_token = os.environ["REFRESH_TOKEN"]
+    user_id = os.environ["USER_ID"]
 
-api = AppPixivAPI()
-api.auth(refresh_token=refresh_token)
-print(api.search_user("example"))
+    api = AppPixivAPI()
+    api.auth(refresh_token=refresh_token)
+    print(api.search_user("example"))
+
+if __name__ == "__main__":
+    main()
