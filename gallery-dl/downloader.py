5e58e061b5978d8d263bb205d7b7b0d3a0b4f27d
Sean S. Williams <mail@seansev.com>
2026-04-24 20:43:16 -0400

    Begin implementing downloading with gallery-dl

---
+++ gallery-dl/downloader.py
@@ -0,0 +1,50 @@
+import sys
+import subprocess
+from pathlib import Path
+
+_BASE_DIR = Path(__file__).parent
+_DEFAULT_VAULT = _BASE_DIR / 'vault'
+
+class GalleryDL:
+    def __init__(self, refresh_token: str, user_id: str, vault_dir: str | None = None):
+        self._refresh_token = refresh_token
+        if not self._refresh_token:
+            raise ValueError("No refresh token provided! Log into Pixiv before attempting to download.")
+
+        self._user_id = user_id
+        if not self._user_id:
+            raise ValueError("No Pixiv user ID provided! Log into Pixiv or regenerate your .env file before attempting to download.")
+
+        self._vault_dir = Path(vault_dir) if vault_dir else _DEFAULT_VAULT
+        if not vault_dir:
+            print(f"WARNING: No vault directory provided! Using {str(_DEFAULT_VAULT)}", file=sys.stderr)
+
+        # Use a custom config and import our refresh token from GPPT
+        self._base_args = [
+            'gallery-dl',
+            '--directory', str(self._vault_dir),
+            '-o', f'cache.file={str(_BASE_DIR / "cache.sqlite3")}',
+            '-o', f'extractor.pixiv.refresh-token={self._refresh_token}',
+            '-o', 'extractor.pixiv.directory=[]',
+            '-o', 'extractor.pixiv.ugoira=copy',
+            '-o', 'extractor.postprocessors=[{"name":"metadata"}]',
+        ]
+
+        # Use separate archive DBs for bookmarks and following. This allows us to use a flat layout while still tricking gallery-dl into redownloading files when they become bookmarked, updating the metadata to reflect this change. Works will never be un-bookmarked in the local copy.
+        self._bookmarks_args = self._base_args + [
+            '--download-archive', str(_BASE_DIR / 'archive_bookmarks.sqlite'),
+            '-o', 'skip=false',
+            f'https://www.pixiv.net/users/{self._user_id}/bookmarks/artworks',
+        ]
+
+        self._following_args = self._base_args + [
+            '--download-archive', str(_BASE_DIR / 'archive_following.sqlite'),
+            '-o', 'skip=true',
+            f'https://www.pixiv.net/users/{self._user_id}/following',
+        ]
+
+    def download_bookmarks(self):
+        subprocess.run(self._bookmarks_args, check=True)
+
+    def download_following(self):
+        subprocess.run(self._following_args, check=True)
---
+++ gallery-dl/main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from dotenv import load_dotenv
+from downloader import GalleryDL
+
+def main():
+    load_dotenv()
+
+    refresh_token = os.getenv("REFRESH_TOKEN")
+    user_id = os.getenv("USER_ID")
+    vault_dir = os.getenv("VAULT_DIR")
+
+    gdl = GalleryDL(
+        refresh_token=refresh_token,
+        user_id=user_id,
+        vault_dir=vault_dir,
+    )
+
+    gdl.download_bookmarks()
+    gdl.download_following()
+
+if __name__ == "__main__":
+    main()
